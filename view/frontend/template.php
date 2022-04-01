211bf5bdaf8aeb72fa1b59fa8bfa132c23ccd98c
Benjamin Peronne <50184355+BenjaminPeronne@users.noreply.github.com>
2022-03-31 20:10:01 -0400

    update `viewport` for mobile

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -4,6 +4,9 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
+    <!-- Title -->
     <title><?= $title ?></title>
 
     <!-- font -->
@@ -24,8 +27,8 @@
 
 <body>
     <?= $content ?>
-    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
